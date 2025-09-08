1ee80949e86ae33fad0ae579d654c6cdf7522b49
Daniel Dawson Jensen <dadj@aarhus.dk>
2025-09-08 09:33:22 +0200

    updated git ignore and pyproject

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ from automation_server_client import AutomationServer, Workqueue, WorkItemError
 
 
 async def populate_queue(workqueue: Workqueue):
+    """Populate the workqueue with items to be processed."""
+
     logger = logging.getLogger(__name__)
 
     logger.info("Hello from populate workqueue!")
 
 
 async def process_workqueue(workqueue: Workqueue):
+    """Process items from the workqueue."""
+
     logger = logging.getLogger(__name__)
 
     logger.info("Hello from process workqueue!")
@@ -19,7 +23,7 @@ async def process_workqueue(workqueue: Workqueue):
     for item in workqueue:
         with item:
             data = item.data  # Item data deserialized from json as dict
- 
+
             try:
                 # Process the item here
                 pass
@@ -32,15 +36,15 @@ async def process_workqueue(workqueue: Workqueue):
 if __name__ == "__main__":
     ats = AutomationServer.from_environment()
 
-    workqueue = ats.workqueue()
+    prod_workqueue = ats.workqueue()
 
     # Initialize external systems for automation here..
 
     # Queue management
     if "--queue" in sys.argv:
-        workqueue.clear_workqueue("new")
-        asyncio.run(populate_queue(workqueue))
+        prod_workqueue.clear_workqueue("new")
+        asyncio.run(populate_queue(prod_workqueue))
         exit(0)
 
     # Process workqueue
-    asyncio.run(process_workqueue(workqueue))
+    asyncio.run(process_workqueue(prod_workqueue))
