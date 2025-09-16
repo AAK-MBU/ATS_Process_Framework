2f97ce2ed38f2e3b69dcd60af29f5a9e94875126
Sebastian <128781428+SebastianHorluck@users.noreply.github.com>
2025-09-16 08:54:20 +0200

    Update application_handler.py

--- processes/application_handler.py
+++ processes/application_handler.py
@@ -2,18 +2,22 @@
 
 
 def startup():
+    """Function for starting applications"""
     return
 
 
 def soft_close():
+    """Function for closing applications softly"""
     return
 
 
 def hard_close():
+    """Function for closing applications hard"""
     return
 
 
 def close():
+    """Function for closing applications softly or hardly if necessary"""
     try:
         soft_close()
     except Exception:
@@ -21,5 +25,6 @@ def close():
 
 
 def reset():
+    """Function for resetting application"""
     close()
     startup()
